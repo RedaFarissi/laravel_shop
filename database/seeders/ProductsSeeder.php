6048abf353c3a47e64551d5544a2b4029d58f7b1
RedaFarissi <redaesskouni@gmail.com>
2023-10-25 10:50:30 +0100

    .

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -14,67 +14,70 @@ use Illuminate\Support\Str;
 use \Illuminate\Support\Facades\File;
 
 class ProductsSeeder extends Seeder{
-
-
     public function run(): void {
         $productData = json_decode(file_get_contents(public_path('products-test/products.json')), true);
          
-        
         $user = User::where('role' , "super admin")->first();
-
-        // create images folder inside storage/app/public/ if not exist
-        if (!Storage::exists('app/public/images')) {
-            Storage::makeDirectory('public/images', 0775, true);
-        }
-
-        foreach ($productData as $item) {
-            //Copy images from public to storage to be readable in .blade.php
-        
-            if(!file_exists(storage_path('app/public/images/'.$item['image'])) ) {
-                copy(
-                    public_path('products-test/images-test/'.$item['image']) , 
-                    storage_path('app/public/images/'.$item['image']) 
-                );
-            } 
-
-            //Check if the category already exists by name
-            $existingCategory = Category::where('name', $item['category'])->first();
-            if ($existingCategory === null) {
-                $category = Category::create(['name'=> $item['category']]);
-                $categoryID = $category->id;
-            }else{
-                $categoryID = $existingCategory->id;
+        if($user) {
+            // create images folder inside storage/app/public/ if not exist
+            if (!Storage::exists('app/public/images')) {
+                exec('php artisan storage:link');
+                Storage::makeDirectory('public/images', 0775, true);
             }
+        
 
-            $existingProduct = Product::where('name', $item['name'])->where('price', $item['price'])->first();
-            if ($existingProduct === null) {
-                $newProduct =  Product::create([
-                    "name"=>$item['name'],
-                    "image"=> $item['image'],
-                    "description"=>$item['description'],
-                    "category_id"=>$categoryID,
-                    "price"=>$item['price'],
-                    "available"=>true,
-                    "user_id"=>$user->id
-                ]);
+            foreach ($productData as $item) {
+                //Copy images from public to storage to be readable in .blade.php
+            
+                if(!file_exists(storage_path('app/public/images/'.$item['image'])) ) {
+                    copy(
+                        public_path('products-test/images-test/'.$item['image']) , 
+                        storage_path('app/public/images/'.$item['image']) 
+                    );
+                } 
 
-                foreach( $item['sizes'] as $size ){
-                    //check if Size in DB and add it if not exist
-                    $existingSize = Size::where('name', $size)->first();
-                    if($existingSize === null){
-                        $sizeCreate =Size::create([ 'name' => $size ]);
-                        $SizeID = $sizeCreate->id;
-                    }else{
-                        $SizeID = $existingSize->id;
-                    }
-                    //pivot table
-                    DB::table('product_size')->insert([
-                        'product_id' => $newProduct->id,
-                        'size_id' => $SizeID
+                //Check if the category already exists by name
+                $existingCategory = Category::where('name', $item['category'])->first();
+                if ($existingCategory === null) {
+                    $category = Category::create(['name'=> $item['category']]);
+                    $categoryID = $category->id;
+                }else{
+                    $categoryID = $existingCategory->id;
+                }
+
+                $existingProduct = Product::where('name', $item['name'])->where('price', $item['price'])->first();
+                if ($existingProduct === null) {
+                    $newProduct =  Product::create([
+                        "name"=>$item['name'],
+                        "image"=> $item['image'],
+                        "description"=>$item['description'],
+                        "category_id"=>$categoryID,
+                        "price"=>$item['price'],
+                        "available"=>true,
+                        "user_id"=>$user->id
                     ]);
+
+                    foreach( $item['sizes'] as $size ){
+                        //check if Size in DB and add it if not exist
+                        $existingSize = Size::where('name', $size)->first();
+                        if($existingSize === null){
+                            $sizeCreate =Size::create([ 'name' => $size ]);
+                            $SizeID = $sizeCreate->id;
+                        }else{
+                            $SizeID = $existingSize->id;
+                        }
+                        //pivot table
+                        DB::table('product_size')->insert([
+                            'product_id' => $newProduct->id,
+                            'size_id' => $SizeID
+                        ]);
+                    }
                 }
             }
+            $this->command->info('Products created successfully.');
+        }else{
+            $this->command->error("You don't have Super Admin Account");
+            $this->command->line(" ''' Run Seeder class first to run it use 'php artisan db:seed --class=SuperAdminSeeder' ''' ");
         }
-        $this->command->info('Products created successfully.');
     }
 }
